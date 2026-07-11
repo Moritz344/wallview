9d428724d421397718cd29b4a5fd4b04844e9449
Moritz344 <moritzmaier34@gmail.com>
2026-07-11 23:18:15 +0200

    chore: remove local and browse component import => use wallpaper component instead

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,8 @@
-import { Component,inject } from '@angular/core';
+import { Component,inject,signal } from '@angular/core';
 import { Topbar } from '../topbar/topbar';
 import { Settings } from '../settings';
-import { Local } from './local/local';
-import { Browse } from './browse/browse';
+import { Wallpaper } from '../wallpaper/wallpaper';
+import { Electron } from '../electron';
 
 // TODO: browse local wallpapers
 // TODO: browse wallhaven wallpapers
@@ -10,13 +10,62 @@ import { Browse } from './browse/browse';
 
 @Component({
   selector: 'app-home',
-  imports: [Topbar,Local,Browse],
+  imports: [Topbar,Wallpaper],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   public settings = inject(Settings);
+  public electron = inject(Electron);
+
   public tab = this.settings.selectedTab;
-  constructor() {}
+
+  public readonly loadQuantity = signal<number>(5);
+
+  public localWallpapers = signal<string[]>([]);
+  public currentLoadedLocalWallpapers = signal<string[]>([]);
+
+  public wallhavenWallpapers = signal<string[]>([]);
+  public currentLoadedWallhavenWallpapers = signal<string[]>([]);
+
+  public isLoading = signal<boolean>(false);
+
+  constructor() {
+    this.initWallpapers();
+  }
+
+  initWallpapers() {
+    if (this.tab() == "local") {
+      this.searchLocalWallpapers();
+    } else {
+      this.searchWallhavenWallpapers();
+    }
+  }
+
+
+  loadMoreLocalWallpapers() {
+    const newLoadedWallpapers = this.localWallpapers().slice(0,this.currentLoadedLocalWallpapers().length + this.loadQuantity())
+    this.currentLoadedLocalWallpapers.set(newLoadedWallpapers);
+  }
+
+  loadMoreWallhavenWallpapers() {}
+
+  onLoadMoreWallpapers() {
+    if (this.tab() == "local") {
+      this.loadMoreLocalWallpapers();
+    } else {
+      this.loadMoreWallhavenWallpapers();
+    }
+
+  }
+
+  async searchLocalWallpapers() {
+    this.isLoading.set(true);
+    this.localWallpapers.set(await this.electron.getLocalWallpapers());
+    this.currentLoadedLocalWallpapers.set(this.localWallpapers().slice(0,this.loadQuantity()));
+    this.isLoading.set(false);
+  }
+
+  searchWallhavenWallpapers() {}
 
 }
