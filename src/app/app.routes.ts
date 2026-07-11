2c31d1aeda843473621187533312212810174857
Moritz344 <moritzmaier34@gmail.com>
2026-07-11 23:16:48 +0200

    chore: remove routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
-import { Local } from './home/local/local';
-import { Browse } from './home/browse/browse';
 
 export const routes: Routes = [
   { path: "",component: Home},
-  { path: "local",component: Local},
-  { path: "browse",component: Browse},
 ];
