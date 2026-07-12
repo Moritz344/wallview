7c7226c21104cdeb1728bc76aedcfb92174336e2
Moritz344 <moritzmaier34@gmail.com>
2026-07-12 16:49:20 +0200

    chore: provide httpclient

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { HttpClientModule } from "@angular/common/http";
+import { importProvidersFrom } from "@angular/core";
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(App, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideRouter(routes)
+  ]
+});
