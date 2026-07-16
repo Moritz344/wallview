8200cb5918b94bee67bac997ac4d10051829de40
Moritz344 <moritzmaier34@gmail.com>
2026-07-16 19:51:41 +0200

    chore: remove http client module,vars and search method

--- src/app/wallhaven.ts
+++ src/app/wallhaven.ts
@@ -1,10 +1,4 @@
-import { Injectable,signal,inject } from '@angular/core';
-import {
-  HttpClient,
-  HttpParams,
-  HttpHeaders,
-  HttpErrorResponse,
-} from "@angular/common/http";
+import { Injectable,signal} from '@angular/core';
 
 
 
@@ -12,9 +6,6 @@ import {
   providedIn: 'root',
 })
 export class Wallhaven {
-  private http = inject(HttpClient);
-  public baseUrl = signal<string>("https://wallhaven.cc/api/v1");
-
   public readonly categories = signal<string[]>(["anime","general","people"])
   public readonly sorting = signal<string[]>(["date_added"," relevance", "random"," views", "favorites", "toplist"])
   public readonly orders = signal<string[]>(["asc","desc"]);
@@ -31,26 +22,6 @@ export class Wallhaven {
   ]);
 
   constructor() {}
-
-  search(searchParameter: any) {
-    // TODO: make api request to search for wallpapers with electron instead
-    const params = new URLSearchParams();
-    params.append("q",searchParameter.q);
-    params.append("sorting",searchParameter.sorting);
-    params.append("order",searchParameter.order);
-    params.append("page",searchParameter.page);
-    params.append("categories",
-          searchParameter.categories
-                  .filter((category: any) => category.isChecked)
-                  .map((c: any) => c.name.toLowerCase())
-                  .join(",")
-    )
-    params.append("resolution",searchParameter.resolution);
-
-    const url = this.baseUrl() + "/search?" + params.toString();
-
-    return this.http.get(url)
-  }
 
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { HttpClientModule } from "@angular/common/http";
-import { importProvidersFrom } from "@angular/core";
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
 
 bootstrapApplication(App, {
   providers: [
-    importProvidersFrom(HttpClientModule),
     provideRouter(routes)
   ]
 });
