33b41385d1ff19ce95d2a97e0d9c5f1fdcd8f8b8
Moritz344 <moritzmaier34@gmail.com>
2026-07-17 18:37:28 +0200

    chore: remove unused import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Topbar } from './topbar/topbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Topbar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
