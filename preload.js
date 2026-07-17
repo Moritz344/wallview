3724f6c5270d960a83ef6f6c67686e0469612269
Moritz344 <moritzmaier34@gmail.com>
2026-07-17 14:32:22 +0200

    feat: expose about window,get about data and close electron window function

--- preload.js
+++ preload.js
@@ -6,5 +6,8 @@ contextBridge.exposeInMainWorld("electronAPI", {
   getLocalWallpapers: () => ipcRenderer.invoke("get-local-wallpapers"),
   openWallpaper: (path) => ipcRenderer.invoke("open-wallpaper",path),
   openExternalLink: (url) => ipcRenderer.invoke("open-external",url),
+  openAboutWindow: () => ipcRenderer.invoke("open-about"),
+  getAboutData: () => ipcRenderer.invoke("get-about-data"),
+  closeElectronWindow: (name) => ipcRenderer.invoke("close-electron-window",name),
   searchWallhaven: (params) => ipcRenderer.invoke("search-wallhaven",params)
 });
