ff3a22b9e579eb1ef91c63fae944f0447bb0f13f
Moritz344 <moritzmaier34@gmail.com>
2026-07-18 17:04:15 +0200

    chore: expose ipc handler

--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   exit: () => ipcRenderer.invoke("exit"),
-  minmize: () => ipcRenderer.invoke("minimize"),
+  minimize: () => ipcRenderer.invoke("minimize"),
   getLocalWallpapers: () => ipcRenderer.invoke("get-local-wallpapers"),
   openWallpaper: (path) => ipcRenderer.invoke("open-wallpaper",path),
   openExternalLink: (url) => ipcRenderer.invoke("open-external",url),
